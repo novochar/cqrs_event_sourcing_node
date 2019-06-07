6916b36684d8036de7668ce8fc01641c309c41cb
matheus antonio da silva <matheusantoniosilva@gmail.com>
2019-06-06 21:51:57 -0300

    Added config of transactions

---
+++ transactions/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
