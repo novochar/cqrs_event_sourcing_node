dc94c865d4a49b6c4e1c1716459ef43606095011
matheus antonio da silva <matheusantoniosilva@gmail.com>
2019-06-14 10:58:11 -0300

    merge license

--- transactions/services/index.js
+++ transactions/services/index.js
@@ -1,17 +1,26 @@
 import express from 'express'
 import RedisSMQ from 'rsmq'
+import bodyParser from 'body-parser'
 
 const rsmq = new RedisSMQ( {host: "queue", port: 6379, ns: "rsmq" } )
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use((req, res, next) => {
+    console.log(req.body)
+    next()
+})
 
 app.get('*', (req, res) => {
-    rsmq.sendMessageAsync({qname:"events", message:"Hello World"}).then(function (resp) {
+    const body_json = JSON.stringify(req.body)
+    rsmq.sendMessageAsync({qname:"events", message:JSON.stringify(req.body)}).then(function (resp) {
         if (resp) {
-            console.log("Message sent. ID:", resp);
+            console.log("Message sent. ID:", resp)
+            console.log("body:" , JSON.stringify(body_json));
         }
     });
-    res.send("ok")
+    res.send(body_json)
 })
 
 const PORT = process.env.PORT || 8080
