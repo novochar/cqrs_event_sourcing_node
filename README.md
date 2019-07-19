f9553b65c8a85d6f100370da62d72e356bb2e6ce
matheus <matheusantoniosilva@gmail.com>
2019-07-19 10:10:10 -0300

    send message to logstash

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# cqrs event sourcing made in node:
+# cqrs event sourcing made in node (incomplete):
 exemple of cqrs event sourcing made in node, docker, docker-compose, elasticsearch and message queue
 
 # dependencies:
 - docker
 - docker-compose
+
+# run:
+
+mkdir ./data
+mkdir ./data/elasticseach
+chmod g+rwx ./data/elasticseach
+docker-compose up
+
+
+# send message of product
+
+send post to localhost:8081/message
--- events/products/index.js
+++ events/products/index.js
@@ -1,5 +1,6 @@
 import RSMQWorker from  "rsmq-worker"
 import RedisSMQ from 'rsmq'
+import axios from 'axios'
 
 const rsmq = new RedisSMQ({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, ns: "rsmq"}),
   worker = new RSMQWorker( "products_events", {alwaysLogErrors: true, rsmq} )
@@ -8,6 +9,11 @@ worker.on( "message", function( msg, next, id ){
 	// process your message
   console.log("Message id : " + id)
   console.log(msg);
+  axios.post('http://logs_stream:3000', { service: 'product', id , msg })
+  .then(function(response){
+    console.log('saved...')
+    console.log(response)
+  }); 
   next()
 })
 
